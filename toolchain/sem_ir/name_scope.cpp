a7df529074e5894f5b1cf1e4d3547c314ac2bea3
Boaz Brickner <brickner@google.com>
2024-12-04 09:31:14 +0100

    Move NameScope::AddRequired() and NameScope::LookupOrAdd() to a cpp file.

---
+++ toolchain/sem_ir/name_scope.cpp
@@ -0,0 +1,35 @@
+// Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "toolchain/sem_ir/name_scope.h"
+
+namespace Carbon::SemIR {
+
+auto NameScope::AddRequired(Entry name_entry) -> void {
+  auto add_name = [&] {
+    int index = names_.size();
+    names_.push_back(name_entry);
+    return index;
+  };
+  auto result = name_map_.Insert(name_entry.name_id, add_name);
+  CARBON_CHECK(result.is_inserted(), "Failed to add required name: {0}",
+               name_entry.name_id);
+}
+
+auto NameScope::LookupOrAdd(SemIR::NameId name_id,
+                            llvm::function_ref<InstId()> make_inst_id,
+                            AccessKind access_kind)
+    -> std::pair<bool, EntryId> {
+  auto insert_result = name_map_.Insert(name_id, names_.size());
+  if (!insert_result.is_inserted()) {
+    return {false, EntryId(insert_result.value())};
+  }
+
+  names_.push_back({.name_id = name_id,
+                    .inst_id = make_inst_id(),
+                    .access_kind = access_kind});
+  return {true, EntryId(names_.size() - 1)};
+}
+
+}  // namespace Carbon::SemIR
--- toolchain/sem_ir/name_scope.h
+++ toolchain/sem_ir/name_scope.h
@@ -73,31 +73,11 @@ class NameScope : public Printable<NameScope> {
     return EntryId(lookup.value());
   }
 
-  // Adds a name to the scope that must not already exist.
-  auto AddRequired(Entry name_entry) -> void {
-    auto add_name = [&] {
-      int index = names_.size();
-      names_.push_back(name_entry);
-      return index;
-    };
-    auto result = name_map_.Insert(name_entry.name_id, add_name);
-    CARBON_CHECK(result.is_inserted(), "Failed to add required name: {0}",
-                 name_entry.name_id);
-  }
+  auto AddRequired(Entry name_entry) -> void;
 
   auto LookupOrAdd(SemIR::NameId name_id,
                    llvm::function_ref<InstId()> make_inst_id,
-                   AccessKind access_kind) -> std::pair<bool, EntryId> {
-    auto insert_result = name_map_.Insert(name_id, names_.size());
-    if (!insert_result.is_inserted()) {
-      return {false, EntryId(insert_result.value())};
-    }
-
-    names_.push_back({.name_id = name_id,
-                      .inst_id = make_inst_id(),
-                      .access_kind = access_kind});
-    return {true, EntryId(names_.size() - 1)};
-  }
+                   AccessKind access_kind) -> std::pair<bool, EntryId>;
 
   auto extended_scopes() const -> llvm::ArrayRef<InstId> {
     return extended_scopes_;
