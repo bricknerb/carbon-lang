492f59460a6121ec71fa51fa70505bf4c3b616a2
Boaz Brickner <brickner@google.com>
2025-08-07 18:09:13 +0200

    Remove debug log.

--- testing/file_test/autoupdate.cpp
+++ testing/file_test/autoupdate.cpp
@@ -393,9 +393,6 @@ auto FileTestAutoupdater::Run(bool dry_run) -> bool {
   if (!dry_run) {
     std::ofstream out(file_test_path_);
     out << new_content;
-  } else {
-    // TODO: Remove.
-    llvm::errs() << "Autoupdate new content:\n" << new_content;
   }
   return true;
 }
