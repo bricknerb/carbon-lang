92f85e6674083469674823eaa6ccfb413db41a1b
Boaz Brickner <brickner@google.com>
2025-09-25 15:21:22 +0200

    Clean up `PerformCallToCppOverloadFunction()` commnt.

--- toolchain/check/cpp/overload_resolution.h
+++ toolchain/check/cpp/overload_resolution.h
@@ -11,25 +11,21 @@
 
 namespace Carbon::Check {
 
-// Checks and builds SemIR for a call to `callee_id` with self `self_id` and
-// arguments `args_id`, where the callee is a C++ (potentially overloaded)
-// function.
+// Checks and builds SemIR for a call to a C++ function in the given overload
+// set with self `self_id` and arguments `arg_ids`.
 //
-// Performs overloading resolution over the overload set. A set with a single
-// non-templated function goes through the same rules for overloading
-// resolution. Uses Clang to find the best viable function for the call.
+// Chooses the best viable C++ function by performing Clang overloading
+// resolution over the overload set.
 //
-// Preserves the given self, if set. If not set, and the function is a a C++
+// Preserves the given self, if set. If not set, and the function is a C++
 // member operator, self will be set to the first argument, which in turn will
 // be removed from the given args.
 //
-// Note on non-overloaded functions: In C++, a single non-templated function is
-// also treated as an overloaded set and goes through the overload resolution to
-// ensure that the function is viable for the call. This is to make sure that
-// calls that have no viable implicit conversion sequence are rejected even when
-// an implicit conversion is possible. Keeping the same behavior here for
-// consistency and supporting migrations so that the migrated callers from C++
-// remain valid.
+// A set with a single non-templated function goes through the same rules for
+// overloading resolution. This is to make sure that calls that have no viable
+// implicit conversion sequence are rejected even when an implicit conversion is
+// possible. Keeping the same behavior here for consistency and supporting
+// migrations so that the migrated callers from C++ remain valid.
 auto PerformCallToCppOverloadFunction(Context& context, SemIR::LocId loc_id,
                                       SemIR::CppOverloadSetId overload_set_id,
                                       SemIR::InstId self_id,
